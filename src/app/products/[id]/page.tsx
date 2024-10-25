13c4e0cd263176d1c3e09f0d364bcf039e48cf51
abderrahimRa <khobiziziabdou@gmail.com>
2024-10-25 13:58:31 +0100

    back to 0

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect } from "react";
-import { useRouter } from "next/router";
+"use client";
+import React, { useState, useRef } from "react";
 import products from "@/Data/products";
 import NavBar from "@/components/ui/NavBar";
 import { ChevronLeft } from "lucide-react";
@@ -10,34 +10,11 @@ import ProductDetails from "@/components/ui/ProductDetails";
 import HomeFooter from "@/components/ui/HomeFooter";
 import { StaticImageData } from "next/image";
 
-// Function to generate static paths for each product ID
-export async function generateStaticParams() {
-  return products.map((product) => ({
-    id: product.id.toString(), // Ensure IDs are strings
-  }));
-}
-
-// Main component
-const ProductPage: React.FC = () => {
+export default function ProductPage({ params }: { params: { id: string } }) {
   const [mousePosition, setMousePosition] = useState({ x: 0.5, y: 0.5 });
-  const [product, setProduct] = useState<{
-    id: number;
-    name: string;
-    price: string;
-    description: string;
-    image: string | StaticImageData | (string | StaticImageData)[];
-  } | null>(null);
   const productRef = useRef<HTMLDivElement>(null);
-  const router = useRouter();
-  const { id } = router.query;
 
-  useEffect(() => {
-    if (id) {
-      const productId = parseInt(id as string);
-      const fetchedProduct = products.find((p) => p.id === productId);
-      setProduct(fetchedProduct || null);
-    }
-  }, [id]);
+  const product = products.find((p) => p.id === parseInt(params.id));
 
   const handleMouseMove = (e: React.MouseEvent) => {
     const { clientX, clientY } = e;
@@ -66,8 +43,8 @@ const ProductPage: React.FC = () => {
     <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-200 overflow-x-hidden">
       <BackgroundEffect mousePosition={mousePosition} />
       <NavBar />
-      <main className="relative pt-24 mb-9" onMouseMove={handleMouseMove}>
-        <div className="max-w-7xl mx-auto grid grid-cols-1 px-4">
+      <main className="relative pt-24 mb-9 " onMouseMove={handleMouseMove}>
+        <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 px-4">
           <div className="mb-6 flex items-center text-gray-600">
             <Link href="/" className="hover:text-gray-900 transition-colors">
               Home
@@ -90,6 +67,4 @@ const ProductPage: React.FC = () => {
       </footer>
     </div>
   );
-};
-
-export default ProductPage;
+}
